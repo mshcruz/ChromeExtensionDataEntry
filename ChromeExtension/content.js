a238e17804ca85f1689884028cb3060de01616b4
Mateus Cruz <mshcruz@gmail.com>
2021-03-16 14:45:27 +0900

    Transfer name->ID mapping to client side

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -11,11 +11,33 @@ chrome.runtime.onMessage.addListener((message) => {
 // Wait until register button is enabled before entering data
 function registerWhenReady(registerButton, client) {
   if (registerButton.disabled) {
-    setTimeout(() => { registerWhenReady(registerButton, client); }, 500);
+    setTimeout(() => {
+      registerWhenReady(registerButton, client);
+    }, 500);
   } else {
-    for (const [key, value] of Object.entries(client)) {
-      document.getElementById(key).value = value;
+    const columnNameElementIDMap = {
+      "Code": 'code',
+      "Name": 'name',
+      "Address": 'address',
+      "Country": 'country',
+      "Type": 'type',
+      'Discount Percentage': 'discount',
+      'Credit Limit': 'limit',
+      'Credit Status': 'status',
+      'Payment Terms': 'terms',
+      "Language": 'language',
+      "Currency": 'currency',
+      "Active": 'active',
+    };
+    for (let [key, value] of Object.entries(client)) {
+      const elementID = columnNameElementIDMap[key];
+      if (elementID === 'active') {
+        document.getElementById(elementID).checked = (value === 'YES');
+      } else {
+        document.getElementById(elementID).value = value;
+      }
+
     }
     registerButton.click();
   }
-} 
+}
--- GoogleAppsScript/Clients.js
+++ GoogleAppsScript/Clients.js
@@ -2,13 +2,6 @@ function doGet(e) {
   const clientsSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Clients');
   const clientsData = clientsSheet.getDataRange().getValues();
 
-  // Replace columns' names to match the fields in the form
-  clientsData[0] =
-    ['code', 'name', 'address',
-      'country', 'type', 'discount',
-      'limit', 'status', 'terms',
-      'language', 'currency', 'active'];
-
   return ContentService.createTextOutput(JSON.stringify(biArrayToObject(clientsData)));
 }
 
